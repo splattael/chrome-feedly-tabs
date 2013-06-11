d78c9d89a06e89a08adfc7e37e5ec93a99c6f753
Peter Suschlik <ps@neopoly.de>
2013-06-11 15:08:38 +0200

    Add some docs.

--- js/background.js
+++ js/background.js
@@ -1,7 +1,15 @@
+/**
+ * Execute tabs.js in context of current tab.
+ */
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, { file: "js/tabs.js" });
 });
 
+/**
+ * Message listener triggered by tabs.js which creates a new inactive tab.
+ *
+ * @params request.url - The URL to be opened in tab.
+ */
 chrome.extension.onMessage.addListener(function(request, sender, response) {
   chrome.tabs.create({ url: request.url, active: false });
 });
