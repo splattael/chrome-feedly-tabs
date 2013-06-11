fbd7595ef16027a2600844696f32dbae9cc4ca88
Peter Suschlik <ps@neopoly.de>
2013-06-11 15:18:21 +0200

    Refactor tabs.js.

--- js/tabs.js
+++ js/tabs.js
@@ -1,8 +1,18 @@
 (function() {
-  var links = document.querySelectorAll("div[data-alternate-link]");
-  for (var i = 0, size = links.length; i < size; i++) {
-    var link = links[i];
-    var url = link.getAttribute("data-alternate-link");
-    chrome.extension.sendMessage({ url: url });
+  /**
+   * Iterates over each link found on a Feedly page emiting its url to +callback+.
+   */
+  function eachFeedlyUrl(callback) {
+    var link_attribute = "data-alternate-link";
+    var links = document.querySelectorAll("div[" + link_attribute + "]");
+    for (var i = 0, size = links.length; i < size; i++) {
+      var link = links[i];
+      var url = link.getAttribute(link_attribute);
+      callback(url);
+    }
   }
+
+  eachFeedlyUrl(function(url) {
+    chrome.extension.sendMessage({ url: url });
+  });
 })();
