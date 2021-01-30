b088b75623e1eb101fe340b085499ffc2dd27c9a
Peter Leitzen <peter@leitzen.de>
2021-01-30 15:07:36 +0100

    Make extension compatible with the latest Feedly version
    Fixes https://mobile.twitter.com/dsush/status/1354603724346454016

--- js/tabs.js
+++ js/tabs.js
@@ -4,7 +4,7 @@
    */
   function eachFeedlyUrl(callback) {
     var link_attribute = "data-alternate-link";
-    var links = document.querySelectorAll("div[" + link_attribute + "]");
+    var links = document.querySelectorAll("article[" + link_attribute + "]");
     for (var i = 0, size = links.length; i < size; i++) {
       var link = links[i];
       var url = link.getAttribute(link_attribute);
